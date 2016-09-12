eb437504956637f4f1b999dc4e4688cfcbba5cae
Pavel <19paul91@mail.ru>
2016-09-13 00:10:44 +0300

    мап кит лессон комплект

--- MapOnFly/MapOnFly/ViewController.swift
+++ MapOnFly/MapOnFly/ViewController.swift
@@ -11,13 +11,45 @@ import MapKit
 
 class ViewController: UIViewController {
 	
+	//позволяет менять координаты
+	var geokoder: CLGeocoder!
+	
 	@IBOutlet weak var textField: UITextField!
 	@IBOutlet weak var mapField: MKMapView!
 	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		geokoder = CLGeocoder()
+		//указываем какой объект для наблюдения мы добавляем UIControlEvents.EditingChanged
+		//и следим если изменения происходят то отправляем в self
+		textField.addTarget(self, action: "textFieldChanged", forControlEvents: UIControlEvents.EditingChanged)
 		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	//метод для изменения
+	func textFieldChanged(){
+		
+		geokoder.geocodeAddressString((textField.text!)) { (placemarks, error) -> Void in
+			if error != nil{
+				print("MapKit error \(error?.description)")
+			}
+			
+			if placemarks != nil {
+				if let placemark = placemarks?.first{
+					
+					let annotation = MKPointAnnotation()
+					annotation.title = "Просмотр места"
+					annotation.subtitle = self.textField.text
+					annotation.coordinate = (placemark.location?.coordinate)!
+					
+					self.mapField.showAnnotations([annotation], animated: true)
+					//self.mapField.selectedAnnotations(annotation, animated: true)
+				}
+			}
+			
+		}
+		
 	}
 
 	override func didReceiveMemoryWarning() {
